9d962fcc459a2ae994b3f1009b367506a302e000
Cynnah <massot.dorian@gmail.com>
2019-05-18 12:20:15 +0200

    creation on add button to burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Auxilliary from '../../components/Layout/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/Build Controls/BuildControls';
 
+const INGREDIENT_PRICES = { // <--- *** CONSTANTES GLOBALES ***
+    salad: 0.5,
+    cheese:0.7,
+    meat: 2,
+    bacon: 0.6
+}
+
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -15,14 +22,33 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 7
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]; // Besoin de savoir combien il y a d'ingredients
+        const updatedCounted = oldCount + 1; // J'augmente de 1 un ingredients 
+        const updatedIngredients = {
+            ...this.state.ingredients // Je répartis les propriétés de l'ancien etat des ingredients dans le nouvel objet que je crée ici
+        };
+        updatedIngredients[type] = updatedCounted // Je prends mon objet updated ingredient et je definis le nombre la valeur correspondant au nombre d'ingredient = a updatedCount
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render () {
         return (
             <Auxilliary>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}/>
             </Auxilliary>
         );
     }
