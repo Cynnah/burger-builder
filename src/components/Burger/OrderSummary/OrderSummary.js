62a0521e1348fb9487ebc6dc83760373e9ca2dbc
Cynnah <massot.dorian@gmail.com>
2019-05-23 15:04:37 +0200

    improving performances

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxilliary from '../../Layout/Auxilliary';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functionnal component, doesn't have to be a class
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         });
 
-    return (
-        <Auxilliary>
-            <h3>Votre Commande</h3>
-            <p>Un Hamburger délicieux avec les ingrédients suivants :</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Prix Total : {props.price.toFixed(2)}</strong></p>
-            <p>Continuer la commande?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>ANNULER</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUER</Button>
-        </Auxilliary>
-    );
+        return(
+            <Auxilliary>
+                <h3>Votre Commande</h3>
+                <p>Un Hamburger délicieux avec les ingrédients suivants :</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Prix Total : {this.props.price.toFixed(2)}</strong></p>
+                <p>Continuer la commande?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>ANNULER</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUER</Button>
+            </Auxilliary>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
